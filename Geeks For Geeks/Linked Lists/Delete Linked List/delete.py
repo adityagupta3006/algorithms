8cc5e0421c2d7f119f5526be7b09b25cde5662ef
agupta42 <agupta42@uncc.edu>
2018-03-02 01:43:41 -0500

    added completely delete a linked list

--- Geeks For Geeks/Linked Lists/Delete Linked List/delete.py
+++ Geeks For Geeks/Linked Lists/Delete Linked List/delete.py
@@ -1,47 +1,35 @@
 class Node(object):
-	"""docstring for Node"""
+	"""docstring for node"""
 	def __init__(self, data):
 		self.data = data
 		self.next = None
 
 class LinkedList(object):
-	"""docstring for LnkedList"""
+	"""docstring for node"""
 	def __init__(self):
 		self.head = None
-		
+
 	def push(self, new_data):
 		new_node = Node(new_data)
 		new_node.next = self.head
 		self.head = new_node
 
-	def reverse(self):
-		# 1>2>3>4
-		# 4>3>2>1
-		prev = None
-		current = self.head
-		if current is None:
-			return
-		while current:
-			temmp = current.next
-			current.next = prev
-			prev = current
-			current = temp
-		self.head = prev
-	
 	def printList(self):
 		h = self.head
 		while(h):
 			print h.data
 			h = h.next
 
+	def delete(self):
+		self.head = None
+
 llist = LinkedList()
 llist.push(20)
 llist.push(4)
 llist.push(15)
 llist.push(85)
- 
 print "Given Linked List"
 llist.printList()
-llist.reverse()
-print "\nReversed Linked List"
+llist.delete()
+print  "After deletion:"
 llist.printList()
