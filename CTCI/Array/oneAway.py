1c35fd2dbe7747335603061f8085a72e5f43e6e8
agupta42 <agupta42@uncc.edu>
2018-03-02 23:48:54 -0500

    oneAway ctci

--- CTCI/Array/oneAway.py
+++ CTCI/Array/oneAway.py
@@ -1,3 +1,9 @@
+'''
+There are three types of edits that can be performed on strings: insert a character,
+remove a character, or replace a character. Given two strings, write a function to check if they are
+one edit (or zero edits) away. 
+'''
+
 def oneAway(a1, a2):
 	d1 = {}
 	for i in a1:
@@ -19,12 +25,28 @@ def oneAway(a1, a2):
 		for a in d1:
 			if a not in d2:
 				diff += 1
-			if diff>2:
-				return False
 		
 	
+	if len(d1) < len(d2):
+		diff = 0 
+		for i in d2:
+			if i not in d1:
+				diff += 1
+			if diff>2:
+				return False
+	
+	if len(d2) < len(d1):
+		diff = 0 
+		for i in d1:
+			if i not in d2:
+				diff += 1
+	if diff>1:
+		return False
+	else:
+		return True
+
 
 str1 = "pale"
-str2 = "plea"
+str2 = "bake"
 
 print oneAway(str1, str2)
