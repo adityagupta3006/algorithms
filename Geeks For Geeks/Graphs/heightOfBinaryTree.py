eaad3cbb455974020218219d7fd415e59500e6f6
agupta42 <agupta42@uncc.edu>
2018-03-11 19:48:26 -0400

    tree length

--- Geeks For Geeks/Graphs/heightOfBinaryTree.py
+++ Geeks For Geeks/Graphs/heightOfBinaryTree.py
@@ -13,59 +13,28 @@ def calcHeight(node):
     q = []
     q.append(node)
     
-    while (q):
-        height = len(q)
-        current = q.pop()
-        
-        if current.left:
-            q.append(current.left)        
+    while True:
+        l = len(q)
+        if l == 0:
+            return height
 
-        if current.right:
-            q.append(current.right)
+        height += 1
 
+        while l > 0:
+            current = q.pop()
+            if current.left:
+                q.append(current.left)
+            
+            if current.right:
+                q.append(current.right)
 
-
-
-root = Node(1)
-root.left = Node(2)
-root.right = Node(3)
-root.left.left = Node(4)
-root.left.right = Node(5)
-calcHeight(root)
-
-
-'''
-def treeHeight(node):
-    if node is None:
-        return
-
-        q = []
-        q.append(node)
-        h = 0
-
-        while(True):
-            nodeCOunt = len(q)
-            if nodeCOunt == 0:
-                return height
-
-            height += 1
-
-            while (nodeCOunt > 0):
-                node = q[0]
-                q.pop(0)
-                if node.left is not None:
-                    q.append(node.left)
-                if node.right is not None:
-                    q.append(node.right)
-
-                nodeCount -= 1
+            l -= 1
 
 root = Node(1)
 root.left = Node(2)
 root.right = Node(3)
 root.left.left = Node(4)
 root.left.right = Node(5)
-
-print "Height of tree is", treeHeight(root)
-
-'''
+#root.right.left = Node(6)
+#root.right.right = Node(7)
+print calcHeight(root)
