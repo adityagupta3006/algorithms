1c521e51420a8ecadbd19eb3e78e246f90499b4d
agupta42 <agupta42@uncc.edu>
2018-03-12 15:06:23 -0400

    identical linked list added - corrected

--- Geeks For Geeks/Linked Lists/Identical Linked Lists/identicalRecursive.py
+++ Geeks For Geeks/Linked Lists/Identical Linked Lists/identicalRecursive.py
@@ -20,28 +20,17 @@ class LinkedList(object):
 			h = h.next
 
 def identical(node1, node2):
-	n1 = node1 
-	n2 = node2 
-	while n1 and n2:
-
-		if n1.data != n2.data:
-			return False
-
-		n1 = n1.next
-		n2 = n2.next
-
-	return ((n1 is None) and (n2 is None)) 
+	
 
 
 ll1 = LinkedList()
-ll1.push(12)
+#ll1.push(12)
 ll1.push(1)
 ll1.push(2)
 ll1.push(3)
-#ll1.push(4)
-#ll.printList()
+
+
 ll2 = LinkedList()
-#ll2.push(4)
 ll2.push(1)
 ll2.push(2)
 ll2.push(3)
