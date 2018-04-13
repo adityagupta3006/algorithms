14487f69161553cbc33cf4e3b50ea7a5084c0125
agupta42 <agupta42@uncc.edu>
2018-04-12 21:02:34 -0400

    updates

--- Geeks For Geeks/Linked Lists/Print Reverse - Recursion/print.py
+++ Geeks For Geeks/Linked Lists/Print Reverse - Recursion/print.py
@@ -13,6 +13,14 @@ class LinkedList:
 		new_node.next = self.head
 		self.head = new_node
 
+	def printReverse(self, node):
+		if node == None:
+			return
+
+		self.printReverse(node.next)
+
+		print(node.data),
+
 	def insertAfter(self, prevNode, new_data):
 		if prevNode is None:
 			return
@@ -35,14 +43,14 @@ class LinkedList:
 		while(temp):
 			print temp.data,
 			temp = temp.next
-			
+
 if __name__=='__main__':
  
     # Start with the empty list
     llist = LinkedList()
  
     # Insert 6.  So linked list becomes 6->None
-    llist.append(6)
+    llist.push(6)
  
     # Insert 7 at the beginning. So linked list becomes 7->6->None
     llist.push(7);
@@ -51,10 +59,7 @@ if __name__=='__main__':
     llist.push(1);
  
     # Insert 4 at the end. So linked list becomes 1->7->6->4->None
-    llist.append(4)
- 
-    # Insert 8, after 7. So linked list becomes 1 -> 7-> 8-> 6-> 4-> None
-    llist.insertAfter(llist.head.next, 8)
- 
-    print 'Created linked list is:',
+    llist.push(4)
     llist.printList()
+    
+    llist.printReverse(llist.head)
