1312f655ee3b904c5ae6977f7ed32c5cb798da4d
agupta42 <agupta42@uncc.edu>
2018-04-13 19:52:18 -0400

    fibonacci dynamic

--- Geeks For Geeks/Algorithms/fibonacci_dynamic.py
+++ Geeks For Geeks/Algorithms/fibonacci_dynamic.py
@@ -1,2 +1,16 @@
 def fib(n):
-	
+	a = 0
+	b = 1
+	if n<0:
+		return 
+	elif n == 0:
+		return a
+	elif n == 1:
+		return b
+	else:
+		for i in range(2,n):
+			c = a+b
+			a = b
+			b = c
+		return b
+print fib(10)
